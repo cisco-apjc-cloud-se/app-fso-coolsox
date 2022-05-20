8fa878f3862449743fc18d0a591921c6373ade86
Richard <richwats@cisco.com>
2022-05-20 17:34:51 +1000

    try helm package and repo

--- application/helm/package.sh
+++ application/helm/package.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export COPYFILE_DISABLE=true
 helm lint ./coolsox
-tar -czf coolsox-0.0.1.tgz coolsox
+helm package coolsox #tar -czf coolsox-0.0.1.tgz coolsox
 helm repo index ./
